ca3da0bd804db6af18306473f713fbcc32213e9b
Rebeca Lins <rebeca.lsleao@gmail.com>
2022-10-21 18:15:03 -0300

    Requisito 3

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Title extends Component {
   render() {
@@ -6,5 +7,6 @@ class Title extends Component {
     return <h2>{headline}</h2>;
   }
 }
+Title.propTypes = { headline: PropTypes.string.isRequired };
 
 export default Title;
