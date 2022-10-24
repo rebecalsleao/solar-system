a668c8cbe84cd99612d703c571b84f147461a4cc
Rebeca Lins <rebeca.lsleao@gmail.com>
2022-10-24 10:53:22 -0300

    Requisito 4

--- src/components/PlanetCard.js
+++ src/components/PlanetCard.js
@@ -7,11 +7,14 @@ class PlanetCard extends Component {
     return (
       <div data-testid="planet-card">
         <p data-testid="planet-name">{planetName}</p>
-        <img src={planetImage} alt={`Planeta ${planetName}`} />
+        <img src={ planetImage } alt={ `Planeta ${planetName}` } />
       </div>
     );
   }
 }
-PlanetCard.propTypes = { planetName: PropTypes.string.isRequired, planetImage: PropTypes.string.isRequired };
+PlanetCard.propTypes = {
+  planetName: PropTypes.string.isRequired,
+  planetImage: PropTypes.string.isRequired,
+};
 
 export default PlanetCard;
