d1c5be951a94460603c5ca5b0b08e6d15d752ec3
Rebeca Lins <rebeca.lsleao@gmail.com>
2022-10-24 09:20:54 -0300

    Requisito 4

---
+++ src/components/Missions.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class Missions extends Component {
+  render() {
+    return ();
+  }
+}
+
+export default SolarSystem;
+const { headline }
--- src/components/Title.js
+++ src/components/Title.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 class Title extends Component {
   render() {
     const { headline } = this.props;
+    console.log(this.props);
     return <h2>{headline}</h2>;
   }
 }
